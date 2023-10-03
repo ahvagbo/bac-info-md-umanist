4790da5dff32ea5034379344ea7fa08172156e17
Iurie Jurja <iuriejurja@hotmail.com>
2023-10-03 12:21:28 +0300

    Adaugare TDS8.pas

---
+++ teme/tipuri_de_date_structurate/TDS8.pas
@@ -0,0 +1,29 @@
+{
+  Se da un tablou liniar cu n componente numere intregi si un numar intreg m.
+  Determinati daca printe valorile componentelor se contine valoarea data m.
+}
+
+program TDS8;
+
+var a       : array [1..100] of integer;
+    n       : 1..100;
+    m, i, t : integer;
+
+begin
+   write('n = '); readln(n);
+   for i := 1 to n do
+   begin
+      write('a[', i, '] = '); readln(a[i]);
+   end;
+
+   write('m = '); readln(m);
+
+   for i := 1 to n do
+   begin
+      if a[i] = m then t := t + 1;
+   end;
+
+   if t = 0 then writeln('Numarul ', m, ' nu se regaseste in tablou')
+   else if t = 1 then writeln('Numarul ', m, ' se regaseste o singura data')
+                 else writeln('Numarul ', m, ' se regaseste in tablou de ', t, ' ori');
+end.
